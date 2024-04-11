9344341e20f51d31a9554105da940610240d8ddb
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-04-11 21:59:30 +0800

    Revert "Modn't"
    This reverts commit 81cc2b0157384354310bad19cbf19563c822f49a.

---
+++ src/blui/BottomLeftUI.java
@@ -0,0 +1,19 @@
+package blui;
+
+import arc.*;
+import arc.util.*;
+import blui.ui.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class BottomLeftUI extends Mod{
+
+    public BottomLeftUI(){
+    }
+
+    @Override
+    public void init(){
+        BLSetup.init();
+    }
+}
