e9a64e5f71a42c7d093a42cb04676396fb821c90
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-04-11 22:13:45 +0800

    init after other mods add

--- src/blui/BottomLeftUI.java
+++ src/blui/BottomLeftUI.java
@@ -14,6 +14,6 @@ public class BottomLeftUI extends Mod{
 
     @Override
     public void init(){
-        BLSetup.init();
+        Core.app.post(BLSetup::init); //Wait until mods add tables
     }
 }
