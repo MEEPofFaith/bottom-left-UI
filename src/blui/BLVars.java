d6d6ad2bef583b193dcd5b8fc57b8ca6e7236009
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-04-14 21:17:36 -0700

    Long press time setting

--- src/blui/BLVars.java
+++ src/blui/BLVars.java
@@ -1,7 +1,13 @@
 package blui;
 
+import arc.*;
+
 public class BLVars{
     public static float pressTimer;
-    public static float longPress = 20;
+    public static float longPress = 30;
     public static float iconSize = 40f, buttonSize = 24f, sliderWidth = 140f, fieldWidth = 80f;
+
+    protected static void init(){
+        longPress = Core.settings.getInt("blui-longpress", 30);
+    }
 }
--- src/blui/BottomLeftUI.java
+++ src/blui/BottomLeftUI.java
@@ -3,9 +3,11 @@ package blui;
 import arc.*;
 import arc.util.*;
 import blui.ui.*;
+import mindustry.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
+import mindustry.world.meta.*;
 
 public class BottomLeftUI extends Mod{
 
@@ -14,6 +16,9 @@ public class BottomLeftUI extends Mod{
 
     @Override
     public void init(){
+        BLVars.init();
+        Vars.ui.settings.game.sliderPref("blui-longpress", 30, 15, 180, 15, s -> Strings.autoFixed(s / 60f, 2) + " " + StatUnit.seconds.localized());
+
         Core.app.post(BLSetup::init); //Wait until mods add tables
     }
 }
