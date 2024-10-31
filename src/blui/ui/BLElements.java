1d934cca6fd184714033283c818ddb79fa5fc29f
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-10-31 10:56:30 -0700

    Use vanilla-style tooltips

--- src/blui/ui/BLElements.java
+++ src/blui/ui/BLElements.java
@@ -2,6 +2,7 @@ package blui.ui;
 
 import arc.func.*;
 import arc.graphics.*;
+import arc.math.geom.*;
 import arc.scene.*;
 import arc.scene.style.*;
 import arc.scene.ui.*;
@@ -10,7 +11,10 @@ import arc.scene.ui.TextField.*;
 import arc.scene.ui.layout.*;
 import arc.util.*;
 import blui.*;
-import mindustry.gen.*;
+import mindustry.core.*;
+import mindustry.ui.*;
+
+import static mindustry.Vars.*;
 
 public class BLElements{
     public static Element[] sliderSet(Table t, Cons<String> changed, Prov<String> fieldText, TextFieldFilter filter, TextFieldValidator valid, float min, float max, float step, float def, Cons2<Float, TextField> sliderChanged, String title, String tooltip){
@@ -21,9 +25,7 @@ public class BLElements{
         TextField f = t.add(field).left().padLeft(6f).width(BLVars.fieldWidth).get();
 
         if(tooltip != null){
-            Tooltip tip = new Tooltip(to -> to.background(Tex.button).add(tooltip)){{
-                allowMobile = true;
-            }};
+            Tooltip tip = BLElements.tooltip(tooltip);
             tab.addListener(tip);
             sl.addListener(tip);
             f.addListener(tip);
@@ -60,7 +62,7 @@ public class BLElements{
                 l.setColor(b.isDisabled() ? Color.lightGray : Color.white);
             });
         }
-        if(tooltip != null) boxTooltip(b, tooltip);
+        if(tooltip != null) ui.addDescTooltip(b, tooltip);
 
         return bCell;
     }
@@ -98,14 +100,45 @@ public class BLElements{
     }
 
     public static void boxTooltip(Element e, Prov<CharSequence> text){
-        e.addListener(new Tooltip(t -> t.background(Tex.button).label(text)){{
-            allowMobile = true;
-        }});
+        e.addListener(tooltip(text));
     }
 
+    /** @deprecated Just use Vars.ui.addDescTooltip */
     public static void boxTooltip(Element e, String text){
-        e.addListener(new Tooltip(t -> t.background(Tex.button).add(text)){{
-            allowMobile = true;
-        }});
+        ui.addDescTooltip(e, text);
+    }
+
+    /** Yoink from {@link UI#addDescTooltip(Element, String)} */
+    public static Tooltip tooltip(Prov<CharSequence> text){
+        return new Tooltip(t -> t.background(Styles.black8).margin(4f).label(text).color(Color.lightGray)){
+            {
+                allowMobile = true;
+            }
+            @Override
+            protected void setContainerPosition(Element element, float x, float y){
+                this.targetActor = element;
+                Vec2 pos = element.localToStageCoordinates(Tmp.v1.set(0, 0));
+                container.pack();
+                container.setPosition(pos.x, pos.y, Align.topLeft);
+                container.setOrigin(0, element.getHeight());
+            }
+        };
+    }
+
+    /** Yoink from {@link UI#addDescTooltip(Element, String)} */
+    public static Tooltip tooltip(String text){
+        return new Tooltip(t -> t.background(Styles.black8).margin(4f).add(text).color(Color.lightGray)){
+            {
+                allowMobile = true;
+            }
+            @Override
+            protected void setContainerPosition(Element element, float x, float y){
+                this.targetActor = element;
+                Vec2 pos = element.localToStageCoordinates(Tmp.v1.set(0, 0));
+                container.pack();
+                container.setPosition(pos.x, pos.y, Align.topLeft);
+                container.setOrigin(0, element.getHeight());
+            }
+        };
     }
 }
