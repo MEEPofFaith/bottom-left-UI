8a7e600d239cd1b8265e5b5084f9b0d0721a44d7
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-12-10 02:07:56 -0800

    Convert to not a mod

--- src/blui/BLVars.java
+++ src/blui/BLVars.java
@@ -5,7 +5,7 @@ import arc.*;
 public class BLVars{
     public static float pressTimer;
     public static float longPress = 30;
-    public static float iconSize = 40f, buttonSize = 24f, sliderWidth = 140f, fieldWidth = 80f;
+    public static float iconSize = 40f, sliderWidth = 140f, fieldWidth = 80f;
 
     protected static void init(){
         longPress = Core.settings.getInt("blui-longpress", 30);
--- src/blui/ui/BLSetup.java
+++ src/blui/ui/BLSetup.java
@@ -1,30 +1,49 @@
 package blui.ui;
 
-import arc.*;
 import arc.func.*;
+import arc.scene.*;
 import arc.scene.ui.layout.*;
 import arc.struct.*;
 import arc.util.*;
 import blui.*;
 import blui.scene.ui.*;
-import mindustry.game.EventType.*;
+import mindustry.*;
 import mindustry.gen.*;
+import mindustry.world.meta.*;
 
 import static mindustry.Vars.*;
 
 public class BLSetup{
-    private static final Seq<TableData> tables = new Seq<>();
-    private static final Table cont = new Table();
-    private static boolean init;
-    private static int current = -1;
-    private static boolean folded;
+    private static BLUITable bluiTable;
+
+    private static BLUITable getBluiTable(){
+        if(bluiTable != null) return bluiTable;
+
+        //Already added to UI.
+        Element e = ui.hudGroup.find("blui");
+        if(e instanceof BLUITable t){
+            return bluiTable = t;
+        }
+
+        //Create for the first time. Also add setting here.
+        Vars.ui.settings.game.sliderPref("blui-longpress", 30, 15, 180, 15, s -> Strings.autoFixed(s / 60f, 2) + " " + StatUnit.seconds.localized());
+
+        BLUITable all = new BLUITable();
+        all.bottom().left();
+        all.name = "blui";
+        all.setOrigin(Align.bottomLeft);
+        all.defaults().bottom().left();
+        ui.hudGroup.addChild(all);
+
+        return bluiTable = all;
+    }
 
     public static void addTable(Cons<Table> t){
-        tables.add(new TableData(t));
+        getBluiTable().tables.add(new TableData(t));
     }
 
     public static void addTable(Cons<Table> t, Boolp visible){
-        tables.add(new TableData(t, visible));
+        getBluiTable().tables.add(new TableData(t, visible));
     }
 
     /**
@@ -35,19 +54,15 @@ public class BLSetup{
         if(mobile) table.moveBy(0f, Scl.scl(46f));
     }
 
-    public static void init(){
-        if(!init){
-            init = true;
-            BLStyles.init();
+    private static class BLUITable extends Table{
+        private final Seq<TableData> tables = new Seq<>();
+        private final Table cont = new Table();
+        private int current = -1;
+        private boolean folded;
 
-            Table all = new Table().bottom().left();
-            all.name = "blui";
-            all.setOrigin(Align.bottomLeft);
-            all.defaults().bottom().left();
-            all.add(cont);
-            next();
-
-            all.table(Tex.buttonEdge3, t -> {
+        public BLUITable(){
+            add(cont);
+            table(Tex.buttonEdge3, t -> {
                 HoldImageButton b = new HoldImageButton(Icon.refresh, BLStyles.bluiHoldImageStyle);
                 b.clicked(() -> {
                     if(folded){
@@ -57,7 +72,7 @@ public class BLSetup{
                     next();
                 });
                 b.held(() -> {
-                    clear();
+                    clearTable();
                     folded = true;
                 });
                 b.canHold(() -> !folded);
@@ -68,47 +83,36 @@ public class BLSetup{
 
                 t.add(b);
             }).update(t -> checkVisibility());
-            all.visible(BLSetup::visible);
-
-            ui.hudGroup.addChild(all);
-            offset(all);
-
-            Events.on(WorldLoadEndEvent.class, e -> {
-                if(visible()) checkVisibility();
-            });
+            visible(() -> ui.hudfrag.shown && !ui.minimapfrag.shown() && hasVisible());
         }
-    }
 
-    /** If current table is not visible, switch to next one. */
-    private static void checkVisibility(){
-        if(tables.any() && !tables.get(current).visible() && hasVisible()) next();
-    }
+        private void next(){
+            if(tables.isEmpty()) return;
 
-    private static boolean visible(){
-        return ui.hudfrag.shown && !ui.minimapfrag.shown() && hasVisible();
-    }
-
-    private static boolean hasVisible(){
-        return tables.contains(TableData::visible);
-    }
-
-    private static void next(){
-        if(tables.isEmpty()) return;
-
-        current = (current + 1) % tables.size;
-        TableData table = tables.get(current);
-        if(table.visible()){
-            clear();
-            cont.defaults().bottom().left();
-            table.table.get(cont);
-        }else{
-            next();
+            current = (current + 1) % tables.size;
+            TableData table = tables.get(current);
+            if(table.visible()){
+                clearTable();
+                cont.defaults().bottom().left();
+                table.table.get(cont);
+            }else{
+                next();
+            }
         }
-    }
 
-    private static void clear(){
-        cont.clear();
-        cont.background(null);
+        private void clearTable(){
+            cont.clear();
+            cont.background(null);
+        }
+
+        /** If current table is not visible, switch to next one. */
+        private void checkVisibility(){
+            if(tables.any() && !tables.get(current).visible() && hasVisible()) next();
+        }
+
+        private boolean hasVisible(){
+            return tables.contains(TableData::visible);
+        }
     }
 
     private static class TableData{
