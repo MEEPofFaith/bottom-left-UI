7ceeae154ea7c00216343787529da35595b16219
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-12-10 02:58:06 -0800

    no assets

--- src/blui/BLVars.java
+++ src/blui/BLVars.java
@@ -3,11 +3,7 @@ package blui;
 import arc.*;
 
 public class BLVars{
-    public static float pressTimer;
+    public static float pressTimer = Core.settings.getInt("blui-longpress", 30);
     public static float longPress = 30;
-    public static float iconSize = 40f, sliderWidth = 140f, fieldWidth = 80f;
-
-    protected static void init(){
-        longPress = Core.settings.getInt("blui-longpress", 30);
-    }
+    public static float iconSize = 40f, buttonSize = 24f, sliderWidth = 140f, fieldWidth = 80f;
 }
